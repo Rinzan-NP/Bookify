ed1369552dc78cc5d72f487e96a5728fb602b91e
Rinzan-NP <nprinzan@gmail.com>
2024-02-14 18:33:08 +0530

    login page modified

--- client/src/components/User/Auth/Auth.jsx
+++ client/src/components/User/Auth/Auth.jsx
@@ -5,9 +5,9 @@ import Logo from "../../Logo";
 const Auth = () => {
     return (
         <>
-            <div className="min-h-screen bg-[#fefff0]">
+            <div className="min-h-screen bg-[#f0f3cd]">
                 {/* Top */}
-                <div className="p-5 pl-16 py-0 flex">
+                <div className="p-5  py-0 flex top-bar">
                     <Logo width="140px" height="140px" />
                     <div
                         className="items-center flex justify-center"
@@ -52,7 +52,9 @@ const Auth = () => {
                 </div>
                 {/* Main */}
                 <div className="flex justify-center items-center ">
-                    <div className="w-1/2" id="desktop-decoration"></div>
+                    <div className="w-1/2" id="desktop-decoration">
+                        
+                    </div>
                     {/* Input box */}
                     <div className="">
                         <div className="flex justify-center items-center pos">
@@ -60,7 +62,6 @@ const Auth = () => {
                                 {/* Icon */}
                                 <div className="flex justify-center mb-6">
                                     <svg
-                                    
                                         width="120"
                                         height="120"
                                         viewBox="0 0 120 120"
@@ -175,17 +176,17 @@ const Auth = () => {
                                         class="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer text-blue-gray-700 outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 border-t-transparent focus:border-t-transparent border-blue-gray-200 focus:border-gray-900"
                                         placeholder=""
                                     />
-                                    <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
+                                    <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-blue-900">
                                         Email
                                     </label>
                                 </div>
                                 <div class="relative h-10 w-72 min-w-[200px]  bg-slate-50 mb-5">
                                     <input
                                         type="password"
-                                        class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                                        class="peer h-full w-full rounded-[7px] border border-bg-blue-900 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                                         placeholder=" "
                                     />
-                                    <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                                    <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-500">
                                         Password
                                     </label>
                                 </div>
@@ -193,7 +194,7 @@ const Auth = () => {
                                 <div className="flex mb-4">
                                     <a
                                         href=""
-                                        className="text-sm font-semibold shadow-sm ml-1"
+                                        className="text-sm font-bold shadow-sm ml-1 text-blue-900 hover:text-blue-600"
                                     >
                                         Forgot password ?
                                     </a>
@@ -202,26 +203,27 @@ const Auth = () => {
                                 <div className="">
                                     <button
                                         type="button"
-                                        className="text-[#FFDD5D] font-semibold bg-blue-900 px-10 py-2   border-2 rounded-xl w-full mt-2 border-solid"
+                                        className="text-[#FFDD5D] font-semibold bg-blue-900 px-10 py-2   border-2 rounded-xl w-full mt-2 border-solid hover:bg-blue-700"
                                     >
                                         Login
                                     </button>
                                 </div>
                                 <div className="mt-4">
                                     <div class="flex justify-center gap-3">
-                                        <div class="flex justify-center items-center bg-blue-900 px-4 py-2 w-full rounded-lg text-[#FFDD5D] font-semibold">
+                                        <div class="flex justify-center items-center bg-blue-900 px-4 py-2 w-full rounded-lg text-[#FFDD5D] font-semibold hover:bg-blue-800">
                                             <i class="fab fa-google"></i>&nbsp;
                                             Google
                                         </div>
-                                        <div class="flex justify-center items-center bg-blue-900 px-4 py-2 w-full rounded-lg text-[#FFDD5D] font-semibold">
+                                        <div class="flex justify-center items-center bg-blue-900 px-4 py-2 w-full rounded-lg text-[#FFDD5D] font-semibold hover:bg-blue-800">
                                             <i class="fab fa-facebook"></i>
                                             &nbsp; Facebook
                                         </div>
                                     </div>
                                 </div>
                                 <div className="flex justify-center">
-                                    <button className="mt-3 bg-[#FFDD5D] px-10 py-2 rounded-xl text-blue-800 font-semibold">
-                                        Signup
+                                    <div className="flex items-center text-center text-sm font-bold text-blue-900 justify-center mt-3">Dont have an account ? &nbsp;&nbsp;&nbsp; </div>
+                                    <button className="mt-3 bg-[#FFDD5D] px-10 py-2 rounded-xl text-blue-800 font-semibold hover:bg-yellow-400">
+                                        Register
                                     </button>
                                 </div>
                             </div>
