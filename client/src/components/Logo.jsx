0a6a454f8fa0828faf1ffc22fb5f2452b05a89d9
Rinzan-NP <nprinzan@gmail.com>
2024-02-14 10:15:55 +0530

    Setup

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Logo = (props) => {
+    const {width, height} = props;
+  return (
+    <img style={{width:width,height:height}} src="https://i.imgur.com/FeufZQ0.png" alt="logo" />
+  )
+}
+
+export default Logo
