efd3daf38ea954c14ffa751316f8bbccf1f8b83a
Rinzan-NP <nprinzan@gmail.com>
2024-02-19 22:38:54 +0530

    notification started

--- client/src/pages/User/Home.jsx
+++ client/src/pages/User/Home.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import TopBar from "../../components/User/TopBar/TopBar";
 import SideNavBar from "../../components/Sidebar/SideNavBar";
-import "../User/User.css"
+import "../User/User.css";
 import MobileNav from "../../components/Sidebar/MobileNav";
+import Post from "../../components/Posts/Post";
 
 const Home = () => {
     return (
         <>
             <TopBar />
-            <div className="flex px-14 py-12 side">
-                <SideNavBar/>
+            <div className="flex px-14 py-12 side gap-7">
+                <SideNavBar />
+                <div className="middle-box ">
+                    <div></div>
+                    <div>
+                        <Post />
+                    </div>
+                </div>
             </div>
-            <MobileNav/>
-
+            <MobileNav />
         </>
     );
 };
