c40a51a16bb4939866e6c3fd75ec34da2a577f27
Rinzan-NP <nprinzan@gmail.com>
2024-02-26 15:41:39 +0530

    auth started

--- Auth/api/models.py
+++ Auth/api/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("Please enter an email address")
+        if not password:
+            raise ValueError("Please enter a password")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)
+        user.save(using=self.db)
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.verified = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+
+class User(AbstractBaseUser, BaseModel):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    blocked = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+    
+    def full_name(self):
+        return f'{self.username}' 
+    
+    def has_perm(self, perm, obj = None):
+        return self.is_superuser
+    
+
